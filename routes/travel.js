769e59397f71c61030b51312711c06c80721e3d2
Laury Baillon <laury-baillon@hotmail.fr>
2024-11-05 12:19:59 +0100

    ajout route get travels

--- routes/travel.js
+++ routes/travel.js
@@ -1,7 +1,29 @@
+const mongoose = require('mongoose');
 var express = require('express');
 var router = express.Router();
-const fetch = require('node-fetch');cd
+//const fetch = require('node-fetch');
+const Travel = require("../models/travel")
 
+// route Get pour récupérer tout les voyages dispo pour un départ et une arrivée donnée en fonction de la date
+router.get("/travel/:departure/:arrival/:date", (req,res) => {
+    const travelDep= req.params.departure
+    const travelArr = req.params.arrival
+    const dateDep = req.params.date
+    Travel.find({
+        departure:  { $regex: new RegExp(travelDep, 'i') },
+        arrival: { $regex: new RegExp(travelArr, 'i') },
+        date: dateDep
+    }).then(tarvelsFound => {
+        if (tarvelsFound){
+            res.json({result: true, travels: tarvelsFound})
+        }else {
+            res.json({result: false, error: "No trip found."})
+        }
+    })
+})
 
+router.get("/", (req,res) => {
+    Travel.find().then(data => {res.json({travels : data})})
+})
 
 module.exports = router;
