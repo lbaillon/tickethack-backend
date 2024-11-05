99d17865831e519d27e2231ef29f592a61629509
Laury Baillon <laury-baillon@hotmail.fr>
2024-11-05 14:30:45 +0100

    ajout route get by destination arrival and date, ajout clés cart et booking dans db

--- routes/travel.js
+++ routes/travel.js
@@ -1,29 +1,39 @@
-const mongoose = require('mongoose');
-var express = require('express');
+const mongoose = require("mongoose");
+var express = require("express");
 var router = express.Router();
 //const fetch = require('node-fetch');
-const Travel = require("../models/travel")
+const Travel = require("../models/travel");
+var moment = require("moment");
+
 
 // route Get pour récupérer tout les voyages dispo pour un départ et une arrivée donnée en fonction de la date
-router.get("/travel/:departure/:arrival/:date", (req,res) => {
-    const travelDep= req.params.departure
-    const travelArr = req.params.arrival
-    const dateDep = req.params.date
-    Travel.find({
-        departure:  { $regex: new RegExp(travelDep, 'i') },
-        arrival: { $regex: new RegExp(travelArr, 'i') },
-        date: dateDep
-    }).then(tarvelsFound => {
-        if (tarvelsFound){
-            res.json({result: true, travels: tarvelsFound})
-        }else {
-            res.json({result: false, error: "No trip found."})
-        }
-    })
-})
+router.get("/:departure/:arrival/:date", (req, res) => {
+  const travelDep = req.params.departure;
+  const travelArr = req.params.arrival;
+  const travelDate = moment(req.params.date).format("YYYY-MM-DD"); //formatage de la date passée en paramètre
+  Travel.find({
+    //pour ressortir un tableau de tout les travels avec départ et arrivée passé en paramètre
+    departure: { $regex: new RegExp(travelDep, "i") },
+    arrival: { $regex: new RegExp(travelArr, "i") },
+  }).then((tarvelsFound) => {
+    if (tarvelsFound.length === 0) {
+      res.json({ result: false, error: "No trip found." });
+    } else {
+      //filtre du tableau avec la date passée en paramètre : formatage de la date dans les travels trouvés
+      const travelsOnDate = tarvelsFound.filter(
+        (travel) => moment(travel.date).format("YYYY-MM-DD") === travelDate
+      );
+      res.json({ result: true, travels: travelsOnDate });
+    }
+  });
+});
 
-router.get("/", (req,res) => {
-    Travel.find().then(data => {res.json({travels : data})})
-})
+
+// route GET pour récupérer tout les voyages
+router.get("/", (req, res) => {
+  Travel.find().then((data) => {
+    res.json({ travels: data });
+  });
+});
 
 module.exports = router;
