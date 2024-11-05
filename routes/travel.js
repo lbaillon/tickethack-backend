e6fc1e48feeb9c8b70d832a4cf2548fbf05250f1
Amandine <amandinejprat@gmail.com>
2024-11-05 12:23:35 +0100

    comitting travel route

--- routes/travel.js
+++ routes/travel.js
@@ -1,7 +1,19 @@
 var express = require('express');
 var router = express.Router();
-const fetch = require('node-fetch');cd
+//const fetch = require('node-fetch');cd
+const Travel = require("../models/travel")
 
+
+router.post('/', (req, res) => {
+    const newTravel = new Travel ({
+      departure: req.body.departure,
+      arrival: req.body.arrival,
+      date: req.body.date,
+      price: req.body.price,
+      cart: req.body.cart,
+    })
+    newTravel.save().then(data => res.json({ allTravels: [data] }));
+    });
 
 
 module.exports = router;
